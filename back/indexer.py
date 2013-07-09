5e1f6fbe2da9750fa15d6f9f5636eb069ab71811
Sergey Polzunov <sergey@polzunov.com>
2013-07-09 21:22:24 +0000

    adding in-reply and references

--- back/indexer.py
+++ back/indexer.py
@@ -5,6 +5,8 @@ import iso8601
 import json
 import solr
 
+from utils import solr_escape
+
 from jsonschema import validate
 
 
@@ -19,21 +21,19 @@ def get_schema():
 SCHEMA = get_schema()
 
 
-
 def read_blob(filename):
-    json_filename = filename + ".json"
+    json_filename = filename + ".parsed.json"
     if not os.path.exists(json_filename):
         raise Exception("File %s does not exists!" % json_filename)
 
     with open(json_filename, "r") as f:
         blob_str = f.read()
+        blob = json.loads(blob_str)
+        validate(blob, SCHEMA)
+        return blob
 
-        validate(blob_str, SCHEMA)
 
-        return json.loads(blob_str)
-
-
-def process(blob):
+def create_email_doc(blob):
 
     mid = blob["id"]
     received = iso8601.parse_date(blob.get("received"))
@@ -66,10 +66,40 @@ def process(blob):
     }
 
     print "Pushing to index: %s" % document
+    return document
 
-    solrClient.add(**document)
+
+def get_thread(blob):
+
+    mid = blob["id"]
+    reply_to = blob["reply-to"]
+    references = blob["references"] or []
+
+    parents = [reply_to] + references
+
+    if not parents:
+        return
+
+    query = " ".join(["+child:%s" % solr_escape(p) for p in parents])
+
+    response = solrClient.query(query)
+
+    print response
+
+    for r in response.results:
+        print r
+
+
+def process(filename):
+
+    message_blob = read_blob(filename)
+    email_doc = create_email_doc(message_blob)
+
+    get_thread(message_blob)
+
+
+    #solrClient.add(**document)
     #solrClient.commit()
-
 
 
 if __name__ == '__main__':
@@ -78,7 +108,6 @@ if __name__ == '__main__':
         print "Usage: %s <filepath>" % __file__
         sys.exit(1)
 
-    message_blob = read_blob(sys.argv[1])
-    process(message_blob)
+    process(sys.argv[1])
 
 
