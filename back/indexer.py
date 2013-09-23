98914973bc93677f296e7000b9b1ba733a4205a8
Sergey Polzunov <sergey@polzunov.com>
2013-09-23 13:23:03 +0000

    tweaking indexer

--- back/indexer.py
+++ back/indexer.py
@@ -4,11 +4,18 @@ import os
 import iso8601
 import json
 import solr
+import logging
+
+import sqlite3
 
 from utils import solr_escape
 
 from jsonschema import validate
 
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+sql = sqlite3.connect('/home/curie/curie/users.db')
 
 solrClient = solr.SolrConnection('http://localhost:8983/solr')
 
@@ -32,18 +39,26 @@ def read_blob(filename):
         validate(blob, SCHEMA)
         return blob
 
+def get_recepients(blob):
+    return [a["email"] for a in blob["fields"]["to"]] + [a["email"] for a in blob["fields"]["cc"]] + [a["email"] for a in blob["fields"]["bcc"]]
 
-def create_email_doc(blob):
+
+
+def create_email_doc(account_hash, blob):
 
     mid = blob["id"]
     received = iso8601.parse_date(blob.get("received"))
 
     _from = blob["fields"]["from"][0]
 
+    labels = ["inbox"]
+
     document = {
         "id" : mid,
         "received" : received,
-        "labels" : blob["labels"],
+        "labels" : labels,
+
+        "account" : account_hash,
 
         "message_id" : blob["fields"]["message_id"],
 
@@ -65,23 +80,25 @@ def create_email_doc(blob):
         "body" : [b.get("value") for b in blob["fields"].get("body")]
     }
 
-    print "Pushing to index: %s" % document
     return document
 
 
-def get_thread(blob):
+def get_threads(account_hash, blob):
 
     mid = blob["id"]
-    reply_to = blob["reply-to"]
-    references = blob["references"] or []
 
-    parents = [reply_to] + references
+    references = blob.get("references", [])
+    reply_to = blob.get("in-reply-to", None)
+
+    parents = references + ([reply_to] if reply_to else [])
 
     if not parents:
+        logger.info("No parents for mid=%s" % mid)
         return
 
-    query = " ".join(["+child:%s" % solr_escape(p) for p in parents])
+    query = ("+(%s) " % " OR ".join(["child:'%s'" % solr_escape(p) for p in parents])) + " +account:'%s'" % account_hash
 
+    print query
     response = solrClient.query(query)
 
     print response
@@ -89,14 +106,37 @@ def get_thread(blob):
     for r in response.results:
         print r
 
+    return response.results or None
+
 
 def process(filename):
 
+
     message_blob = read_blob(filename)
-    email_doc = create_email_doc(message_blob)
 
-    get_thread(message_blob)
+    mid = message_blob["id"]
 
+    hashes = []
+
+    for email in get_recepients(message_blob):
+        cur = sql.cursor()
+        found = cur.execute("select hash from accounts, emails where emails.account_id = accounts.id and emails.email = '%s'" % email).fetchone()
+        if found:
+            hashes.append(found[0])
+
+    if not hashes:
+        logger.error("No hashes found for %s", filename)
+        return
+
+    for h in hashes:
+        email_doc = create_email_doc(h, message_blob)
+        thread_docs = get_threads(h, message_blob)
+
+        logger.warn("Email: %s...", str(email_doc)[:150])
+
+
+        if not thread_docs:
+            logger.info("No threads found for mid=%s, account=%s", mid, h)
 
     #solrClient.add(**document)
     #solrClient.commit()
