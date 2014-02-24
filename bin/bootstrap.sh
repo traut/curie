64670b67e491e05f7802f4e59e758d4d3780d9a1
Sergey Polzunov <sergey@polzunov.com>
2014-02-24 16:34:21 +0100

    typo

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -40,7 +40,7 @@ cp -R $SOLR/example/solr/collection1/conf/* $CURIE_SOLR/messages/conf/
 echo "name=messages" > $CURIE_SOLR/messages/core.properties
 
 cp $CURIE/configs/solr/solr.xml $CURIE_SOLR/
-cp -r$CURIE/configs/solr/messages/* $CURIE_SOLR/messages/conf/
+cp $CURIE/configs/solr/messages/* $CURIE_SOLR/messages/conf/
 
 
 # we're done with preparations. let's configure curie
