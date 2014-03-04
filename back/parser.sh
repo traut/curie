bf6486e696aaded4b68f6c702820fc072fd75e2c
Sergey Polzunov <sergey@polzunov.com>
2014-03-04 17:00:01 +0000

    we need bash; removing unnecessary echo

--- back/parser.sh
+++ back/parser.sh
@@ -1,7 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
 DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
-echo java -cp curie-java/release/*.jar com.heyheylabs.curie.Runner $DIR/schemas $1
 java -cp curie-java/release/*.jar com.heyheylabs.curie.Runner $DIR/schemas $1
 
